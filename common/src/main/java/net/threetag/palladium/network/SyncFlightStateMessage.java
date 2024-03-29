78044f5acb4f7faadf6cbeb55ab442d22c7c1c61
Lucas Bandermann <lucasbandermann@gmail.com>
2024-03-29 20:56:59 +0100

    Fixed flight animation desync in multiplayer

--- common/src/main/java/net/threetag/palladium/network/SyncFlightStateMessage.java
+++ common/src/main/java/net/threetag/palladium/network/SyncFlightStateMessage.java
@@ -1,40 +1,51 @@
 package net.threetag.palladium.network;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.player.Player;
 import net.threetag.palladium.entity.FlightHandler;
 import net.threetag.palladium.entity.PalladiumPlayerExtension;
-import net.threetag.palladiumcore.network.MessageC2S;
 import net.threetag.palladiumcore.network.MessageContext;
+import net.threetag.palladiumcore.network.MessageS2C;
 import net.threetag.palladiumcore.network.MessageType;
+import org.jetbrains.annotations.NotNull;
 
-public class SetFlyingStateMessage extends MessageC2S {
+import java.util.Objects;
 
+public class SyncFlightStateMessage extends MessageS2C {
+
+    private final int entityId;
     private final boolean flying;
 
-    public SetFlyingStateMessage(boolean flying) {
+    public SyncFlightStateMessage(int entityId, boolean flying) {
+        this.entityId = entityId;
         this.flying = flying;
     }
 
-    public SetFlyingStateMessage(FriendlyByteBuf buf) {
+    public SyncFlightStateMessage(FriendlyByteBuf buf) {
+        this.entityId = buf.readInt();
         this.flying = buf.readBoolean();
     }
 
     @Override
-    public MessageType getType() {
-        return PalladiumNetwork.SET_FLYING_STATE;
+    public @NotNull MessageType getType() {
+        return PalladiumNetwork.SYNC_FLYING_STATE;
     }
 
     @Override
     public void toBytes(FriendlyByteBuf buf) {
+        buf.writeInt(this.entityId);
         buf.writeBoolean(this.flying);
     }
 
     @Override
     public void handle(MessageContext context) {
-        if (context.getPlayer() instanceof PalladiumPlayerExtension extension) {
+        var entity = Objects.requireNonNull(Minecraft.getInstance().level).getEntity(this.entityId);
+
+        if (entity instanceof PalladiumPlayerExtension extension) {
             var flight = extension.palladium$getFlightHandler();
             if (this.flying) {
-                var flightType = FlightHandler.getAvailableFlightType(context.getPlayer());
+                var flightType = FlightHandler.getAvailableFlightType((Player) entity);
 
                 if (flightType.isNotNull()) {
                     flight.setFlightType(flightType);
@@ -43,8 +54,7 @@ public class SetFlyingStateMessage extends MessageC2S {
                 flight.setFlightType(FlightHandler.FlightType.NONE);
             }
 
-            context.getPlayer().getAbilities().flying = false;
-            new SyncFlightStateMessage(context.getPlayer().getId(), this.flying).sendToTracking(context.getPlayer());
+            ((Player) entity).getAbilities().flying = false;
         }
     }
 }
