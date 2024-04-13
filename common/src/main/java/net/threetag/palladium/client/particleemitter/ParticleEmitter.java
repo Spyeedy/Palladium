97dae567e17df87fc19a657cf72e6d4da1180dee
Lucas Bandermann <lucasbandermann@gmail.com>
2024-04-13 11:48:41 +0200

    Removed "spread" from particle emitters

--- common/src/main/java/net/threetag/palladium/client/particleemitter/ParticleEmitter.java
+++ common/src/main/java/net/threetag/palladium/client/particleemitter/ParticleEmitter.java
@@ -24,16 +24,14 @@ public class ParticleEmitter {
     private final int amount;
     private final Vector3f offset;
     private final Vector3f offsetRandom;
-    private final Vector3f spread;
     private final Vector3f motion;
     private final Vector3f motionRandom;
 
-    public ParticleEmitter(@Nullable BodyPart anchor, int amount, Vector3f offset, Vector3f offsetRandom, Vector3f spread, Vector3f motion, Vector3f motionRandom) {
+    public ParticleEmitter(@Nullable BodyPart anchor, int amount, Vector3f offset, Vector3f offsetRandom, Vector3f motion, Vector3f motionRandom) {
         this.anchor = anchor;
         this.amount = amount;
         this.offset = offset;
         this.offsetRandom = offsetRandom;
-        this.spread = spread;
         this.motion = motion;
         this.motionRandom = motionRandom;
     }
@@ -43,11 +41,10 @@ public class ParticleEmitter {
         var amount = GsonUtil.getAsIntMin(json, "amount", 1, 1);
         var offset = GsonUtil.getAsVector3f(json, "offset", new Vector3f()).div(16);
         var offsetRandom = GsonUtil.getAsVector3f(json, "offset_random", new Vector3f()).div(16);
-        var spread = GsonUtil.getAsVector3f(json, "spread", new Vector3f()).div(16);
         var motion = GsonUtil.getAsVector3f(json, "motion", new Vector3f());
         var motionRandom = GsonUtil.getAsVector3f(json, "motion_random", new Vector3f());
 
-        return new ParticleEmitter(bodyPart, amount, offset, offsetRandom, spread, motion, motionRandom);
+        return new ParticleEmitter(bodyPart, amount, offset, offsetRandom, motion, motionRandom);
     }
 
     public Vec3 getCenter(AbstractClientPlayer player, float partialTick) {
@@ -66,14 +63,14 @@ public class ParticleEmitter {
         var random = RandomSource.create();
         for (int i = 0; i < this.amount; i++) {
             Vector3f offset = new Vector3f(this.offset).add((random.nextFloat() - 0.5F) * 2F * this.offsetRandom.x, (random.nextFloat() - 0.5F) * 2F * this.offsetRandom.y, (random.nextFloat() - 0.5F) * 2F * this.offsetRandom.z);
-            offset.x += (random.nextFloat() - 0.5F) * this.spread.x * 2;
-            offset.y += (random.nextFloat() - 0.5F) * this.spread.y * 2;
-            offset.z += (random.nextFloat() - 0.5F) * this.spread.z * 2;
 
             Vec3 pos = this.anchor != null ? BodyPart.getInWorldPosition(this.anchor, offset, player, partialTick) :
                     player.getPosition(partialTick).add(0, player.getBbHeight() / 2D, 0).add(offset.x, offset.y, offset.z);
 
-            var motion = new Vector3f(this.motion).add((random.nextFloat() - 0.5F) * 2F * this.motionRandom.x, (random.nextFloat() - 0.5F) * 2F * this.motionRandom.y, (random.nextFloat() - 0.5F) * 2F * this.motionRandom.z);
+            var motion = new Vector3f(this.motion).add(
+                    (random.nextFloat() - 0.5F) * 2F * this.motionRandom.x,
+                    (random.nextFloat() - 0.5F) * 2F * this.motionRandom.y,
+                    (random.nextFloat() - 0.5F) * 2F * this.motionRandom.z);
             level.addParticle(particleOptions, pos.x, pos.y, pos.z, motion.x, motion.y, motion.z);
         }
     }
